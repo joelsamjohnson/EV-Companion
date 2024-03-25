05a481649d1911d1655673458760790814fdf0c5
Joel Sam Johnson <joelsamueljohnson@gmail.com>
2024-03-25 17:20:21 +0530

    service-fixed-by-chinchu

--- ServiceCenters/models.py
+++ ServiceCenters/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
-class Service(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def _str_(self):
-        return self.name
 
 class ServiceCenter(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
     email = models.EmailField()
-    services_offered = models.ManyToManyField(Service, related_name='service')
     need_delivery_boy = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Service(models.Model):
+    service_center = models.ForeignKey(ServiceCenter, on_delete=models.CASCADE, related_name='services')
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
